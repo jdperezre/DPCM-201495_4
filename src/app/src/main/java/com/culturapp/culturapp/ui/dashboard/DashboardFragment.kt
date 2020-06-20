dbc6026855419d847c798962e12f5c7fbde9b808
Sebastian Rendón Montoya <sermont@bancolombia.com.co>
2020-06-19 20:13:02 -0500

    Update

--- src/app/src/main/java/com/culturapp/culturapp/ui/dashboard/DashboardFragment.kt
+++ src/app/src/main/java/com/culturapp/culturapp/ui/dashboard/DashboardFragment.kt
@@ -11,21 +11,12 @@ import androidx.lifecycle.ViewModelProviders
 import com.culturapp.culturapp.R
 
 class DashboardFragment : Fragment() {
-
-    private lateinit var dashboardViewModel: DashboardViewModel
-
+    
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_dashboard, container, false)
     }
 }
--- src/app/src/main/java/com/culturapp/culturapp/ui/notifications/NotificationsViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.culturapp.culturapp.ui.notifications
-
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class NotificationsViewModel : ViewModel() {
-}
