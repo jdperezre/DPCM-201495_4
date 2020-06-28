6ac8f7ad4b38af01d4ee1374f0a2b18460c6b43d
Natalia Estrada <nataliae24@gmail.com>
2020-06-28 01:04:32 -0500

    lists and other improvements

--- src/app/src/main/java/com/culturapp/culturapp/ui/favorites/FavoritesFragment.kt
+++ src/app/src/main/java/com/culturapp/culturapp/ui/favorites/FavoritesFragment.kt
@@ -4,11 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.culturapp.culturapp.R
+import com.culturapp.culturapp.adapters.EventListAdapter
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class DashboardFragment : Fragment() {
 
@@ -19,4 +18,5 @@ class DashboardFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
     }
+
 }
