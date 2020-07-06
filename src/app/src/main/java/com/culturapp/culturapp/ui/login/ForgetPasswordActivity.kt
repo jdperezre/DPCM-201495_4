265234c516b240c29e2c48080619fda72d990788
Sebastian Rendón Montoya <sermont@bancolombia.com.co>
2020-07-05 22:46:08 -0500

    Update Forget

--- src/app/src/main/java/com/culturapp/culturapp/ui/login/ForgetPasswordActivity.kt
+++ src/app/src/main/java/com/culturapp/culturapp/ui/login/ForgetPasswordActivity.kt
@@ -5,17 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputType
 import android.view.Window
+import com.culturapp.culturapp.MainActivity
 import com.culturapp.culturapp.R
 import kotlinx.android.synthetic.main.login.*
 import kotlinx.android.synthetic.main.register.*
 import kotlinx.android.synthetic.main.register.editTextPassword
 import kotlinx.android.synthetic.main.titlebar_basic.*
 
-class ForgetEmailActivity : AppCompatActivity() {
+class ForgetPasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val customTitleSupported =  requestWindowFeature(Window.FEATURE_CUSTOM_TITLE)
-        setContentView(R.layout.forget_email)
+        setContentView(R.layout.forget_password)
 
         if (customTitleSupported) {
             supportActionBar!!.hide()
@@ -28,13 +29,17 @@ class ForgetEmailActivity : AppCompatActivity() {
     }
 
     private fun initControls() {
-        editTextEmail.SetHint(R.string.hint_email)
-        editTextEmail.SetInputType(InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS)
+        editTextPassword.SetHint(R.string.hint_contrasena)
+        editTextPassword.SetInputType(InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD)
+
+        editTextConfirm.SetHint(R.string.hint_confirm)
+        editTextConfirm.SetInputType(InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD)
     }
 
     private fun setupEvents() {
         btnNext.setOnClickListener {
-            startActivity(Intent(this, ForgetTokenActivity::class.java))
+            startActivity(Intent(this, MainActivity::class.java))
+            this.finishAffinity()
             overridePendingTransition(R.anim.leftin, R.anim.leftout)
         }
 
--- src/app/src/main/java/com/culturapp/culturapp/ui/login/ForgetTokenActivity.kt
+++ src/app/src/main/java/com/culturapp/culturapp/ui/login/ForgetTokenActivity.kt
@@ -6,16 +6,17 @@ import android.os.Bundle
 import android.text.InputType
 import android.view.Window
 import com.culturapp.culturapp.R
-import kotlinx.android.synthetic.main.login.*
+import kotlinx.android.synthetic.main.forget_token.*
 import kotlinx.android.synthetic.main.register.*
-import kotlinx.android.synthetic.main.register.editTextPassword
+import kotlinx.android.synthetic.main.register.btnNext
+import kotlinx.android.synthetic.main.register.btnPrevious
 import kotlinx.android.synthetic.main.titlebar_basic.*
 
-class ForgetEmailActivity : AppCompatActivity() {
+class ForgetTokenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val customTitleSupported =  requestWindowFeature(Window.FEATURE_CUSTOM_TITLE)
-        setContentView(R.layout.forget_email)
+        setContentView(R.layout.forget_token)
 
         if (customTitleSupported) {
             supportActionBar!!.hide()
@@ -28,13 +29,13 @@ class ForgetEmailActivity : AppCompatActivity() {
     }
 
     private fun initControls() {
-        editTextEmail.SetHint(R.string.hint_email)
-        editTextEmail.SetInputType(InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS)
+        editTextToken.SetHint(R.string.hint_codigo)
+        editTextToken.SetInputType(InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_VARIATION_PASSWORD)
     }
 
     private fun setupEvents() {
         btnNext.setOnClickListener {
-            startActivity(Intent(this, ForgetTokenActivity::class.java))
+            startActivity(Intent(this, ForgetPasswordActivity::class.java))
             overridePendingTransition(R.anim.leftin, R.anim.leftout)
         }
 
