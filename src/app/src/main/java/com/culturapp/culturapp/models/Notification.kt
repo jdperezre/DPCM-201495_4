5b1c316d552522b6350c33b1c75b85248c227fb5
Natalia Estrada <nataliae24@gmail.com>
2020-07-06 23:37:17 -0500

    api notification

---
+++ src/app/src/main/java/com/culturapp/culturapp/models/Notification.kt
@@ -0,0 +1,40 @@
+package com.culturapp.culturapp.models
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class Notification (
+    @Expose
+    @SerializedName("id")
+    val id: Integer,
+    @Expose
+    @SerializedName("titulo")
+    val titulo: String,
+    @Expose
+    @SerializedName("organizacion")
+    val organizacion: String,
+    @Expose
+    @SerializedName("descripcion")
+    val descripcion: String,
+    @Expose
+    @SerializedName("telefono")
+    val telefono: String,
+    @Expose
+    @SerializedName("direccion")
+    val direccion: String,
+    @Expose
+    @SerializedName("fecha_inicio")
+    val fechaInicio: String,
+    @Expose
+    @SerializedName("hora")
+    val hora: String,
+    @Expose
+    @SerializedName("imagen")
+    val imagen: String,
+    @Expose
+    @SerializedName("idCategoria")
+    val idCategoria: String,
+    @Expose
+    @SerializedName("idEstado")
+    val idEstado: String
+)
--- src/app/src/main/java/com/culturapp/culturapp/ui/notifications/NotificationsFragment.kt
+++ src/app/src/main/java/com/culturapp/culturapp/ui/notifications/NotificationsFragment.kt
@@ -1,60 +1,67 @@
 package com.culturapp.culturapp.ui.notifications
 
+import android.app.ProgressDialog
 import android.os.Bundle
-import android.text.InputType
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.culturapp.culturapp.R
 import com.culturapp.culturapp.adapters.EventListNotificationAdapter
+import com.culturapp.culturapp.api.ApiClient
+import com.culturapp.culturapp.models.Event
 
 import kotlinx.android.synthetic.main.fragment_notifications.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class NotificationsFragment : Fragment() {
 
+    lateinit var progressProgressDialog: ProgressDialog
+    var dataList = ArrayList<Event>()
+    lateinit var adapter: EventListNotificationAdapter
+
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_notifications, container, false)
     }
 
     override fun onResume() {
-        //nameEditText.SetInputType(InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_NORMAL)
-        //nameEditText.SetHint(R.string.hint_custom_edittext);
-        //nameEditText.SetIcon(R.drawable.ic_home_black_24dp)
         super.onResume()
 
-        var adapter = EventListNotificationAdapter(this.requireActivity(), generateData())
-
-        events_notification_listView?.adapter = adapter
-        adapter?.notifyDataSetChanged()
-    }
-
-
-    fun generateData(): ArrayList<EventDto> {
-        var result = ArrayList<EventDto>()
-
-        for (i in 0..10) {
-            var dto: EventDto = EventDto(
-                    (i + 1).toString() + " Titulo Evento",
-                    "Mayo " + (i + 1).toString() + ", 2020",
-                    "Centro de espectáculos salón " + (i + 1).toString(),
-                    "7:00 pm."
-                )
-            result.add(dto)
+        progressProgressDialog = ProgressDialog(this.requireContext(), 0)
+        progressProgressDialog.run {
+            setTitle("Cargando")
+            setContentView(R.layout.progress)
+            setCancelable(false)
+            show()
         }
+        getData()
+    }
 
-        return result
+
+    private fun getData() {
+        val call: Call<List<Event>> = ApiClient.getClient.getEvents()
+        call.enqueue(object : Callback<List<Event>> {
+
+            override fun onResponse(call: Call<List<Event>>?, response: Response<List<Event>>?) {
+                progressProgressDialog.dismiss()
+                dataList.addAll(response!!.body()!!)
+                adapter = EventListNotificationAdapter(this@NotificationsFragment.requireActivity(), response!!.body()!!)
+
+                events_notification_listView.adapter = adapter
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onFailure(call: Call<List<Event>>?, t: Throwable?) {
+                progressProgressDialog.dismiss()
+            }
+
+        })
     }
 }
 
-class EventDto(title: String, date: String,  location: String, hour: String
-) {
-    var title = title
-    var date =  date
-    var hour = hour
-    var location =  location
-}
